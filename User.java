639fb3b53555dd12734ee9e15a5bddaa4cf22a8c
Md. Ashikur Rahman Siyam <149653672+2233642642@users.noreply.github.com>
2024-05-30 13:02:09 +0600

    Add files via upload

---
+++ User.java
@@ -0,0 +1,22 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package com.mycompany.driver;
+public class User {
+    private String name;
+    private String email;
+
+    public User(String name, String email) {
+        this.name = name;
+        this.email = email;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+}
