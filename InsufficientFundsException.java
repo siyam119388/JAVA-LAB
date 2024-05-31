68491224b6233eecc2400dcd9cb17fc757ed7ef0
Md. Ashikur Rahman Siyam <149653672+2233642642@users.noreply.github.com>
2024-05-31 23:28:51 +0600

    Add files via upload

---
+++ InsufficientFundsException.java
@@ -0,0 +1,10 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package com.mycompany.account;
+public class InsufficientFundsException extends Exception {
+    public InsufficientFundsException(String message) {
+        super(message);
+    }
+}
